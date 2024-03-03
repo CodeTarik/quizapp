c5a9543120f7c30afcabf4e4b814ef5f9927e7e7
Tarik Bozueslue <bozueslue_t@outlook.de>
2024-03-03 23:00:14 +0100

    JSON

---
+++ script.js
@@ -0,0 +1,40 @@
+let questions = [
+    {
+        "question": "Wer war Nikola Tesla?",
+        "answer_1": "Ein Bauer",
+        "answer_2": "Ein Elektroingenieur und Erfinder",
+        "answer_3": "Ein Lagerist",
+        "answer_4": "Ein Elektriker",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Wie lange war Frau Merkel im Amt?",
+        "answer_1": "4 Jahre",
+        "answer_2": "8 Jahre",
+        "answer_3": "16 Jahre",
+        "answer_4": "1 Monat",
+        "right_answer": 3
+    },
+
+
+    {
+        "question": "Wer hat Javascript erfunden?",
+        "answer_1": "Ottmar Hitzfeld",
+        "answer_2": "Karl-Heinz Rummenigge",
+        "answer_3": "Zinedine Zidane",
+        "answer_4": "Brendan Eich",
+        "right_answer": 4
+    },
+
+
+
+    {
+        "question": "Wer hat Angular erfunden?",
+        "answer_1": "Apple",
+        "answer_2": "Google",
+        "answer_3": "Github",
+        "answer_4": "Albert Einstein",
+        "right_answer": 2
+    },
+]; 
